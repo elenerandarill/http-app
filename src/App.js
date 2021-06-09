8817d4bb31619e84f44eb62f6170ee9183ac9d50
elener <paulina.wojno@gmail.com>
2021-06-09 19:14:23 +0200

    Refactored code.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpService";
+import config from "./config.json";
 import "./App.css";
-
-
-// Unexpected errors handling
-axios.interceptors.response.use(null, error => {
-  const expectedError = error.response && error.response.status >= 400 && error.response.status < 500;
-
-  if (!expectedError) {
-    console.log("Logging the error", error);
-    alert("An unexpected error occurred.");
-  }
-
-  return Promise.reject(error);
-});
-
-
-const apiEndpoint = "http://jsonplaceholder.typicode.com/posts";
 
 
 class App extends Component {
@@ -26,13 +11,13 @@ class App extends Component {
 
 
   async componentDidMount() {
-      const { data: posts } = await axios.get(apiEndpoint); // zwraca liste postow
+      const { data: posts } = await http.get(config.apiEndpoint); // zwraca liste postow
       this.setState({ posts });
   };
 
   handleAdd = async () => {
     const obj = {title: "a", body: "b"};
-    const {data: post} = await axios.post(apiEndpoint, obj);  // zwraca tego posta wraz z ID
+    const {data: post} = await http.post(config.apiEndpoint, obj);  // zwraca tego posta wraz z ID
     // dodanie posta do zbioru posts
     const posts = [post, ...this.state.posts];
     this.setState({posts});
@@ -40,7 +25,7 @@ class App extends Component {
 
   handleUpdate = async post => {
     post.title = "UPDATED";
-    await axios.put(apiEndpoint + "/" + post.id, post);
+    await http.put(config.apiEndpoint + "/" + post.id, post);
 
     const posts = [...this.state.posts];  // klonowanie posts
     const index = posts.indexOf(post);    // znalezienie indeksu
@@ -64,7 +49,7 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      await axios.delete(apiEndpoint + "/" + post.id);
+      await http.delete(config.apiEndpoint + "/" + post.id);
     } catch (ex) {
       // Expected error
       if (ex.response && ex.response.status === 404)
